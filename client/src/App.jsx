be1401093a61a002f0fba371f06a03dcd36fec51
sumit1642 <sumeetyadav1642@gmail.com>
2025-04-11 17:09:01 +0530

    Added States, and created a function getAllUsers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,40 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+	const [AllUsers, setAllUsers] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [error, setError] = useState(null);
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [formData, setFormData] = useState({
+		userName: "",
+		fullName: "",
+		email: "",
+		role: "",
+	});
+	const [editUserId, setEditUserId] = useState(null);
+	const [submitting, setSubmitting] = useState(false);
+
+	const getAllUsers = async () => {
+		setIsLoading(true); // show a loading indicator before fetching
+		const res = await fetch("/users");
+		if (res.ok) {
+			const data = await res.json();
+			setAllUsers(data.users);
+			setError(null);
+		} else {
+			setError(await res.json().msg);
+			setAllUsers([]); // to prevent keeping the old data,
+		}
+		setIsLoading(false);
+	};
+
+	useEffect(() => {
+		getAllUsers();
+	}, []);
+
+
 	return <> </>;
 }
 
